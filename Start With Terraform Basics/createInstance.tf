2405b6ff4ce041ed0382fe5e9fd8bd3c40dc1113
Yves Guduszeit <yves.guduszeit@gmail.com>
2024-02-19 14:21:11 +0100

    Remove duplicates

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,11 +1,3 @@
-
-provider "aws" {
-    version    = "3.53.0"
-    access_key = "AKIARHMMEFHCMVWERXXD"
-    secret_key = "SECRET_KEY_HERE"
-    region     = "eu-west-3"
-}
-
 resource "aws_instance" "MyFirstInstance" {
   count         = 3
   ami           = "ami-01b32e912c60acdfa"
