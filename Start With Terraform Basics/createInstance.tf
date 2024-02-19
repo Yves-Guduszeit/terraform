d242f4fa2881d2861c8852b825bb90b84e0d5154
Yves Guduszeit <yves.guduszeit@gmail.com>
2024-02-19 14:29:20 +0100

    t2.micro instead of t3.micro

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MyFirstInstance" {
   count         = 3
   ami           = "ami-01b32e912c60acdfa"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "demo-instance-${count.index}"
