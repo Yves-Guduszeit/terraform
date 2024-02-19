71f14cd1bc037be3c01758e975fc51e563b06d50
Yves Guduszeit <yves.guduszeit@gmail.com>
2024-02-19 23:49:27 +0100

    Update variables

--- Terraform Concepts Building Blocks/variables.tf
+++ Terraform Concepts Building Blocks/variables.tf
@@ -1,27 +1,21 @@
-variable "AWS_ACCESS_KEY" {
-    type = string
-    default = "AKIASMSIZOF42P2VUDSZ"
-}
+variable "AWS_ACCESS_KEY" {}
 
 variable "AWS_SECRET_KEY" {}
 
 variable "AWS_REGION" {
-default = "us-east-2"
+  default = "eu-west-3"
 }
 
 variable "Security_Group"{
-    type = list
-    default = ["sg-24076", "sg-90890", "sg-456789"]
+  type = list
+  default = ["sg-050fa3a003526f8f6"]
 }
 
 variable "AMIS" {
-    type = map
-    default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
-    }
+  type = map
+  default = {
+    eu-west-3 = "ami-01b32e912c60acdfa"
+  }
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
